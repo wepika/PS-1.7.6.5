0362d7d1a0172e66dde09abb476dc50ea93fef0e
Lionel <massin.lionel@hotmail.com>
2021-01-21 17:44:29 +0100

    test address formatter

---
+++ tests/Wepika/Unit/AddressFormatterTest.php
@@ -0,0 +1,20 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+use WpkColoco\Wepika\Formatter\AddressFormatter;
+
+class AddressFormatterTest extends TestCase
+{
+
+    public function testMergeAddress(){
+        $street = 'rue des sapins';
+        $number = '17';
+        $box = '501';
+
+        $address = new AddressFormatter();
+
+        $this->assertEquals( 'rue des sapins, 17 501',$address->mergeAddressParts($street, $number, $box), 'erreur');
+    }
+
+}
